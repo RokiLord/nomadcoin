e2edb73cd19dd6b4811a863308154313e8843a5f
RokiLord <youht7290@gmail.com>
2022-07-19 01:33:26 +0900

    praticing golang -func and range

--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func plus(a ...int) int {
+	total := 0
+
+	for _, item := range a {
+		total += item
+	}
+
+	return total
+}
+
 func main() {
-	fmt.Println("It works!")
+	result := plus(2, 3, 4, 5, 2, 3, 45, 5, 21, 4, 1, 34)
+
+	fmt.Println(result)
 }
