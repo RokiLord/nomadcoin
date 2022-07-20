0db2d298738d2b8406a1f82b2ad131cd100dedf2
RokiLord <youht7290@gmail.com>
2022-07-21 02:24:40 +0900

    using templates

--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -9,7 +9,13 @@ import (
 	"github.com/RokiLord/nomadcoin/blockchain"
 )
 
-const port string = ":4000"
+//port
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -19,13 +25,15 @@ type homeData struct {
 //response writer = data that wants to send to users
 func home(rw http.ResponseWriter, r *http.Request) {
 	//parsing home.html to tmpl
-	tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
+
 	//get blocks data
 	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 }
 
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*gohtml"))
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	//error handling
