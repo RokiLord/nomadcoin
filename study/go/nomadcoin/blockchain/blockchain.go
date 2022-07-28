c5af6f000a00b9af6e80a37376963561c0634394
RokiLord <youht7290@gmail.com>
2022-07-29 02:22:22 +0900

    html

--- study/go/nomadcoin/blockchain/blockchain.go
+++ study/go/nomadcoin/blockchain/blockchain.go
@@ -19,39 +19,34 @@ type blockchain struct {
 var b *blockchain
 var once sync.Once
 
-func GetBlockChain() *blockchain {
-	if b == nil {
-		once.Do(func() {
-			b = &blockchain{}
-			b.AddBlock("Genensis Block")
-		})
-	}
-
-	return b
-}
-
 func (b *Block) calculateHash() {
-	Hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
-	b.Hash = fmt.Sprintf("%x", Hash)
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
 }
-
 func getLastHash() string {
-	totalBlocks := len(GetBlockChain().blocks)
+	totalBlocks := len(GetBlockchain().blocks)
 	if totalBlocks == 0 {
 		return ""
 	}
-
-	return GetBlockChain().blocks[totalBlocks-1].Hash
+	return GetBlockchain().blocks[totalBlocks-1].Hash
 }
 
-func createBlock(Data string) *Block {
-	newBlock := Block{Data, "", getLastHash()}
+func createBlock(data string) *Block {
+	newBlock := Block{data, "", getLastHash()}
 	newBlock.calculateHash()
 	return &newBlock
 }
-
-func (b *blockchain) AddBlock(Data string) {
-	b.blocks = append(b.blocks, createBlock(Data))
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+func GetBlockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis")
+		})
+	}
+	return b
 }
 
 func (b *blockchain) AllBlocks() []*Block {
--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -1,41 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"github.com/RokiLord/nomadcoin/blockchain"
-)
-
-//port
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-//response writer = data that wants to send to users
-func home(rw http.ResponseWriter, r *http.Request) {
-	//parsing home.html to tmpl
-
-	//get blocks data
-	data := homeData{"Home", blockchain.GetBlockChain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
+import "github.com/RokiLord/nomadcoin/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*gohtml"))
-	http.HandleFunc("/", home)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	//error handling
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
