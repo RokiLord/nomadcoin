970790f2ac7eeec7aaf7af7cb62e733a676b32f2
RokiLord <youht7290@gmail.com>
2022-08-03 01:02:27 +0900

    setup for rest api with marshal

--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/RokiLord/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/RokiLord/nomadcoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleFunc(err)
+	fmt.Printf("%s", b)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	http.ListenAndServe(port, nil)
+
 }
