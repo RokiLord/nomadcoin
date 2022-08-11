03e52305bfbe1f419dd683deb354c5651026cc73
RokiLord <youht7290@gmail.com>
2022-08-11 09:46:10 +0900

    refactoring

--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -1,74 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-
-	"github.com/RokiLord/nomadcoin/blockchain"
-	"github.com/RokiLord/nomadcoin/utils"
+	"github.com/RokiLord/nomadcoin/explorer"
+	"github.com/RokiLord/nomadcoin/rest"
 )
 
-const port string = ":4000"
-
-type URL string
-
-func (u URL) MarshalText() ([]byte, error) {
-	url := fmt.Sprintf("http://localhost%s%s", port, u)
-	return []byte(url), nil
-}
-
-type URLDescription struct {
-	URL         URL    `json:"url"`
-	Method      string `json:"method"`
-	Description string `json:"description"`
-	Payload     string `json:"payload,omitempty"`
-}
-
-type AddBlockbody struct {
-	Message string
-}
-
-func (u URLDescription) String() string {
-	return "Hello I'm the URL Description"
-}
-
-func documentation(rw http.ResponseWriter, r *http.Request) {
-	data := []URLDescription{
-		{
-			URL:         URL("/"),
-			Method:      "GET",
-			Description: "See Documentation",
-		},
-		{
-			URL:         URL("/blocks"),
-			Method:      "POST",
-			Description: "Add A Block",
-			Payload:     "data:string",
-		},
-	}
-	rw.Header().Add("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(data)
-}
-
-func blocks(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
-
-		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
-
-	case "POST":
-		var addBlockBody AddBlockbody
-		utils.HandleFunc(json.NewDecoder(r.Body).Decode(&addBlockBody))
-		fmt.Println(addBlockBody)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", documentation)
-	http.HandleFunc("/blocks", blocks)
-	fmt.Printf("Listening on http://localhost%s", port)
-	http.ListenAndServe(port, nil)
-
+	explorer.Start(4000)
+	rest.Start(4000)
 }
---
+++ study/go/nomadcoin/rest/rest.go
@@ -0,0 +1,89 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/RokiLord/nomadcoin/blockchain"
+	"github.com/RokiLord/nomadcoin/utils"
+)
+
+var port string
+
+type url string
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+type urlDescription struct {
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+type addBlockbody struct {
+	Message string
+}
+
+func (u urlDescription) String() string {
+	return "Hello I'm the URL Description"
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription{
+		{
+			URL:         url("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "GET",
+			Description: "See All Blocks",
+			Payload:     "data:string",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+		{
+			URL:         url("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See A Block",
+		},
+	}
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(data)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+
+	case "POST":
+		var addBlockBody addBlockbody
+		utils.HandleFunc(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func Start(aPort int) {
+	handler := http.NewServeMux()
+	port = fmt.Sprintf(":%d", aPort)
+	handler.HandleFunc("/", documentation)
+	handler.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+
+}
