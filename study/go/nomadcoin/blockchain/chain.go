e6186e57bc1ce845fbce767ca34d735b6165a21b
RokiLord <youht7290@gmail.com>
2022-08-12 22:45:39 +0900

    blockchain to db

--- study/go/nomadcoin/blockchain/chain.go
+++ study/go/nomadcoin/blockchain/chain.go
@@ -2,6 +2,9 @@ package blockchain
 
 import (
 	"sync"
+
+	"github.com/RokiLord/nomadcoin/db"
+	"github.com/RokiLord/nomadcoin/utils"
 )
 
 type blockchain struct {
@@ -9,10 +12,15 @@ type blockchain struct {
 	Height     int    `json:"height"`
 }
 
+func (b *blockchain) persist() {
+	db.SaveBlockchain(utils.ToBytes(b))
+}
+
 func (b *blockchain) AddBlock(data string) {
-	block := createBlock(data, b.NewestHash, b.Height)
+	block := createBlock(data, b.NewestHash, b.Height+1)
 	b.NewestHash = block.Hash
 	b.Height = block.Height
+	b.persist()
 }
 
 var b *blockchain
--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"github.com/RokiLord/nomadcoin/blockchain"
-)
-
 func main() {
-	blockchain.Blockchain()
+
 }
