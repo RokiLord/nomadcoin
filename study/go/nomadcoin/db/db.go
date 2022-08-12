3fd64dfbd11695e3d781539a3ac6c7ba33d84dc7
RokiLord <youht7290@gmail.com>
2022-08-12 19:39:45 +0900

    saving block in db

--- study/go/nomadcoin/db/db.go
+++ study/go/nomadcoin/db/db.go
@@ -1,31 +1,52 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/RokiLord/nomadcoin/utils"
 	"github.com/boltdb/bolt"
 )
 
 const (
-	dbName      = "blockchain.db"
-	dataBucket  = "data"
-	blockBucket = "blocks"
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
 )
 
 var db *bolt.DB
 
 func DB() *bolt.DB {
 	if db == nil {
-		dbPointer, err := bolt.Open("blockchain.db", 0600, nil)
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
 		db = dbPointer
 		utils.HandleErr(err)
-		err = db.Update(func(tx *bolt.Tx) error {
-			_, err := tx.CreateBucketIfNotExists([]byte(dataBucket))
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
 			utils.HandleErr(err)
-			_, err = tx.CreateBucketIfNotExists([]byte(blockBucket))
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
 			return err
 		})
 		utils.HandleErr(err)
 	}
 
 	return db
+}
+
+func SaveBlock(hash string, data []byte) {
+	fmt.Printf("Saving Blcok %s\nData: %b\n", hash, data)
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(blocksBucket))
+		err := bucket.Put([]byte(hash), data)
+		return err
+	})
+	utils.HandleErr(err)
+}
+
+func SaveBlockchain(data []byte) {
+	err := DB().Update(func(t *bolt.Tx) error {
+		bucket := t.Bucket([]byte(dataBucket))
+		err := bucket.Put([]byte("checkpoint"), data)
+		return err
+	})
+	utils.HandleErr(err)
 }
--- study/go/nomadcoin/main.go
+++ study/go/nomadcoin/main.go
@@ -1,7 +1,9 @@
 package main
 
-import "github.com/RokiLord/nomadcoin/cli"
+import (
+	"github.com/RokiLord/nomadcoin/blockchain"
+)
 
 func main() {
-	cli.Start()
+	blockchain.Blockchain()
 }
